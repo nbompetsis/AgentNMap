f156412e64b786c670ee42a8e91b467f84e7ee3a
Nikolas Bompetsis <nbompetsis@di.uoa.gr>
2015-10-26 02:15:57 +0200

    First commit. \nI created a simple example of python-class test

---
+++ agent/agent.py
@@ -0,0 +1,19 @@
+class AgentNMap:
+    """ This is the AgentNmap class. The agent manipulates the nmap call """
+    data = []
+
+    
+    def __init__(self, name ):
+        self.name = name
+        self.data = [1,2,3,4,5,6,7,8,9,0]
+    
+    def print_agent(self):
+        print("Name ", self.name)
+        print(self.data)
+        
+        
+        
+        
+
+        
+
---
+++ main.py
@@ -0,0 +1,10 @@
+from agent.agent import AgentNMap
+
+
+def main():
+    agent = AgentNMap("First Agent")
+    agent.print_agent()
+
+if __name__ == "__main__":
+    main()
+        
